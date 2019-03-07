38408da61635029112dee383d61257aa71362b4c
AndriiTsymbala <andrii.tsymbala@gmail.com>
2019-03-08 00:15:36 +0200

    HomeTask12/13

---
+++ script.js
@@ -0,0 +1,47 @@
+//TASK 12/1
+ var a = prompt("Значення", "");
+ if(a>0) {
+     alert("1");
+ } 
+ else if(a<0) {
+     alert("-1");
+ }
+ else if(a=0) {
+     alert("0");
+ }
+
+// TASK 12/2
+//  var count = 0; 
+//  while (count < 3) {
+//     console.log( "номер " + (count) + "!" ); count++;}
+
+// TASK 12/3
+// var a=+prompt("a?","");
+// switch (a){
+//     case 0:
+//            alert("0");
+//            break;
+//     case 1: 
+//            alert("1");
+//            break;
+//     case 2: 
+//            alert("2,3");
+//            break;
+//     case 3: 
+//             alert("2,3");
+//             break;
+//   }
+
+// TASK 13/1
+// var goods = ["Стіл","Стілець", "Ручка", "Олівець"];
+// console.log(goods.length);
+// console.log(goods[3]);
+
+// TASK 13/2
+// var styles = ["Джаз", "Блюз"];
+// styles[2] = "Рок-н-Рол";
+// console.log(styles.shift());
+// styles.unshift("Реп", "Реггі");
+// console.log(styles);
+
+
